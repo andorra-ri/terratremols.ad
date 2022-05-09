8a28c760715b21ce611a9948d728a25c74f9cd47
Marc Vilella <mvilella.and@gmail.com>
2022-05-09 13:05:06 +0200

    feat: Complete seisms filters with dates

--- src/components/SeismFilters.vue
+++ src/components/SeismFilters.vue
@@ -1,22 +1,48 @@
 <template>
   <details class="collapsible" open>
     <summary><remix-icon name="equalizer" /> Filters</summary>
-    <input v-model="filters.search" placeholder="Location" type="text">
-    <button class="btn" @click="reset">Reset filters</button>
+    <div class="filters">
+      <label class="block">
+        <span>Location</span>
+        <input
+          v-model="filters.search"
+          placeholder="Enter location name"
+          type="text"
+          class="block">
+      </label>
+      <label>
+        <span>From Date</span>
+        <date-picker
+          v-model="filters.dateMin"
+          :format="format.SHORT_DATE"
+          :not-after="filters.dateMax"
+          size="7" />
+      </label>
+      <label>
+        <span>To Date</span>
+        <date-picker
+          v-model="filters.dateMax"
+          :format="format.SHORT_DATE"
+          :not-before="filters.dateMin"
+          :not-after="new Date()"
+          size="7" />
+      </label>
+      <button class="btn block" @click="reset">Reset filters</button>
+    </div>
   </details>
 </template>
 
 <script>
 import { reactive, watch } from 'vue';
-import { RemixIcon } from '/@/components';
-import { normalize } from '/@/utils';
+import { RemixIcon, DatePicker } from '/@/components';
+import { normalize, format } from '/@/utils';
 import config from '/@/config.yaml';
 
-const INITIAL_STATE = { search: '', dates: [null, new Date()] };
+const INITIAL_STATE = { search: '', dateMin: null, dateMax: new Date() };
 
 export default {
   name: 'SeismFilters',
-  components: { RemixIcon },
+  components: { RemixIcon, DatePicker },
   props: {
     modelValue: {
       type: Function,
@@ -31,11 +57,22 @@ export default {
 
     watch(filters, () => {
       const search = normalize(filters.search);
-      const validator = seism => normalize(seism.location).includes(search);
+      const validator = seism => normalize(seism.location).includes(search)
+        && seism.date > filters.dateMin
+        && seism.date < filters.dateMax;
       emit('update:modelValue', validator);
     }, { immediate: true });
 
-    return { filters, reset };
+    return { filters, reset, format };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.filters {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin: 0 1rem;
+}
+</style>
--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,6 +1,7 @@
 import config from '/@/config.yaml';
 
 export const format = {
+  SHORT_DATE: (date, locale = config.locale) => date?.toLocaleString(locale, config.short_date_format),
   DATE: (date, locale = config.locale) => date?.toLocaleString(locale, config.date_format),
   TIME: (date, locale = config.locale) => date?.toLocaleString(locale, config.time_format),
 };
