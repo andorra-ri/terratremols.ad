987de2046b47faf60b9621e2eed91e319388f8f8
Marc Vilella <mvilella.and@gmail.com>
2022-05-30 13:14:59 +0200

    refactor: Automated formatter generators

--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,9 +1,11 @@
 import config from '/@/config.yaml';
 
 export const format = {
-  SHORT_DATE: (date, locale = config.locale) => date?.toLocaleString(locale, config.short_date_format),
-  DATE: (date, locale = config.locale) => date?.toLocaleString(locale, config.date_format),
-  TIME: (date, locale = config.locale) => date?.toLocaleString(locale, config.time_format),
+  ...Object.entries(config.formats.datetime)
+    .reduce((acc, [name, options]) => {
+      acc[name] = (date, locale = config.locale) => date?.toLocaleString(locale, options);
+      return acc;
+    }, {}),
 };
 
 export const round = (number, decimals = 0) => {
