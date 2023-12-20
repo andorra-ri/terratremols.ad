0dfe9a48176fd69873bf55ffb69c112750f94873
Marc Vilella <mvilella.and@gmail.com>
2023-12-20 16:10:10 +0100

    fix: Make icon size configurable with css custom property

--- src/components/RemixIcon.vue
+++ src/components/RemixIcon.vue
@@ -7,3 +7,14 @@
 <script setup lang="ts">
 const props = defineProps<{ name: string }>();
 </script>
+
+<style scoped>
+.icon {
+  --size: 1em;
+
+  width: var(--size);
+  height: var(--size);
+  vertical-align: middle;
+  display: inline-block;
+}
+</style>
