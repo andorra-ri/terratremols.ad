1e484c417baafc50a646ab70d0f8faff1aee3a23
Marc Vilella <mvilella.and@gmail.com>
2024-01-08 12:18:25 +0100

    feat: Add images loading in map

--- src/composables/map/layer.ts
+++ src/composables/map/layer.ts
@@ -1,6 +1,6 @@
 import { ref, watch, effectScope, onUnmounted } from 'vue';
-import { useGeoJSON } from 'mapbox-composition';
-import type { Map, LayerOptions } from './types';
+import { useGeoJSON, useImages } from 'mapbox-composition';
+import type { Map, LayerOptions, ImageOptions } from './types';
 import type { Deferred } from '/@/utils';
 
 export default (map: Deferred<Map>) => {
@@ -8,8 +8,18 @@ export default (map: Deferred<Map>) => {
     const _options = ref(options); // eslint-disable-line no-underscore-dangle
     const layer = ref<ReturnType<typeof useGeoJSON>>();
 
+    const updateImages = async (images: ImageOptions) => {
+      const _map = await map.promise;
+      const { addImages, removeImages } = useImages(_map);
+      removeImages(Object.keys(images));
+      const { urls, ...imageOptions } = 'urls' in images ? images : { urls: images };
+      await addImages(urls as Record<string, string>, { ...imageOptions, persist: false });
+      _map.once('style.load', () => updateImages(images));
+    };
+
     (async () => {
       const resolved = await map.promise;
+      if (_options.value.images) updateImages(_options.value.images);
       layer.value = useGeoJSON(resolved, _options.value);
     })();
 
--- src/composables/map/types.ts
+++ src/composables/map/types.ts
@@ -14,4 +14,14 @@ export type MapOptions = {
   legend?: LegendControlOptions;
 } & MMapOptions;
 
-export type LayerOptions = MaybeRef<GeoJSONLayerOptions>;
+export type ImageOptions = {
+  urls: Record<string, string>;
+  content?: [number, number, number, number];
+  stretchX?: [number, number][];
+  stretchY?: [number, number][];
+  sdf?: boolean,
+} | Record<string, string>;
+
+export type LayerOptions = MaybeRef<{
+  images?: ImageOptions;
+} & GeoJSONLayerOptions>;
