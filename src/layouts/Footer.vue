f0f07ed2b0a18bff3e5f990e696c180338efef33
Carles Serra <cserra@quarsandorra.com>
2024-12-04 12:46:28 +0100

    86c17rf6p: TRT-Modificar el text "Política de privades" i "Cookies"

--- src/layouts/Footer.vue
+++ src/layouts/Footer.vue
@@ -33,9 +33,9 @@
 
     <aside class="legal">
       <p class="policies">
-        <a href="https://www.ari.ad/images/pdf/ObservatoriRiscSismic/politica-de-privacitat.pdf" target="blank">Política de Privadesa</a>
+        <a href="https://www.ari.ad/images/pdf/ObservatoriRiscSismic/politica-de-privacitat.pdf" target="blank">Política de Privacitat</a>
         &middot;
-        <a href="https://www.ari.ad/images/pdf/ObservatoriRiscSismic/politica-de-cookies.pdf" target="blank">Política de cookies</a>
+        <a href="https://www.ari.ad/images/pdf/ObservatoriRiscSismic/politica-de-cookies.pdf" target="blank">Política de Galetes</a>
         &middot;
         <a href="https://ari.ad/avis-legal" target="blank">Avís Legal</a>
       </p>
