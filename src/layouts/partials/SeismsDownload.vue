cbc4824adb28dde6a57a30d4c85da6e573272682
Carles Serra <cserra@quarsandorra.com>
2024-12-06 12:40:32 +0100

    86c18qfa4: TRT-Text el hover del boto descarregar pdf

--- src/layouts/partials/SeismsDownload.vue
+++ src/layouts/partials/SeismsDownload.vue
@@ -1,16 +1,21 @@
 <template>
-  <button
-    :disabled="!props.seisms.length"
-    @click="download(name)">
-    <RemixIcon name="download" />
-  </button>
+  <div class="tooltip-container">
+    <button
+      :disabled="!props.seisms.length"
+      @click="download(name)">
+      <RemixIcon name="download" />
+    </button>
+    <span class="tooltip-text">{{ message('filters.download_data') }}</span>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { computed, toRef } from 'vue';
-import { useCsv } from '/@/composables';
+import { useCsv, useI10n } from '/@/composables';
 import { RemixIcon } from '/@/components';
 import type { Seism } from '/@/types';
+
+const { message } = useI10n();
 
 const props = defineProps<{
   seisms: Seism[];
@@ -35,12 +40,56 @@ const name = computed(() => {
 </script>
 
 <style lang="scss" scoped>
-button {
-  all: unset;
-  background: #8881;
-  padding: 0.5em 0.65em;
-  border-radius: 0.25em;
+.tooltip-container {
+  position: relative;
+  display: inline-block;
 
-  &:disabled { cursor: not-allowed; }
+
+  button {
+    all: unset;
+    background: #8881;
+    padding: 0.5em 0.65em;
+    border-radius: 0.25em;
+    cursor: pointer;
+    pointer-events: auto;
+
+    &:disabled {
+      cursor: not-allowed;
+    }
+  }
+
+  .tooltip-text {
+    visibility: hidden;
+    opacity: 0;
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    border-radius: 0.25em;
+    padding: 0.5em;
+    position: absolute;
+    top: 125%; /* Posiciona el tooltip per sobre */
+    left: 50%;
+    transform: translateX(-50%);
+    white-space: nowrap;
+    z-index: 10;
+    transition: opacity 0.3s ease;
+
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: 100%;
+      left: 50%;
+      transform: translateX(-50%);
+      transform: rotate(180deg);
+      border-width: 0.5em;
+      border-style: solid;
+      border-color: #333 transparent transparent transparent;
+    }
+  }
+
+  &:hover .tooltip-text {
+    visibility: visible;
+    opacity: 1;
+  }
 }
 </style>
